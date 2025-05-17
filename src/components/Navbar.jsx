16c8c63581b2f1dc00acf53a8537ec8a343aecce
harishnshetty <harisn662@gmail.com>
2025-05-17 11:59:02 +0530

    new

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,7 @@ const Navbar = () => {
     <div className="navbar-container">
       <div className="navbar-content">
         <img src={logo} alt="Logo" className="navbar-logo" />
+        <h1>Harish N Shetty </h1>
       </div>
     </div>
   );
